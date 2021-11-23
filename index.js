6801eb98282ecd28b96855518eda3ff96ba5f7b6
shaikh foysal peyal <sfpeyal@gmail.com>
2021-11-24 02:47:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apvaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        const database = client.db('menu');
+        const ItemsCollection = database.collection('items');
+
+        // GET API
+        app.get('/items', async (req, res) => {
+            const cursor = ItemsCollection.find({});
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+    }
+
+
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
